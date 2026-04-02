aa315c8dcd55a92338b50b48b29a748dee5cb277
Willian-Thrd <business.will-theo@outlook.com>
2026-04-02 18:20:28 -0300

    Projeto Finalizado corrigido

--- obj/Release/net8.0/linux-x64/EncurtadorDownload.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/EncurtadorDownload.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EncurtadorDownload")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d26e3c89b4ec0f07256fabbe193d754cc7d332b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c88b03b5c75528a80f1c049b93dbdd56c8dff1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EncurtadorDownload")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EncurtadorDownload")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
